63e011ea15caffd0746c7c5f43960ce573c86298
admin <admin@admins-MacBook-Pro.local>
2023-04-05 18:03:43 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+
+def display_topics(request):
+    LOT=Topic.objects.all()
+    d={'topics':LOT}
+    return render(request,'display_topics.html',context=d)
+
+def display_webpages(request):
+    LOW=Webpage.objects.all()
+    d={'webpages':LOW}
+    return render(request,'display_webpages.html',d)
+
+def display_access(request):
+    LOA=AccessRecord.objects.all()
+    d={'access':LOA}
+    return render(request,'display_access.html',d)
